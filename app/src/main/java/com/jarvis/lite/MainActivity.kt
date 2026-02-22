27a304a92293d77f458cafec1031443822834a3b
Abdukayumov <90976864+Abdukayumov@users.noreply.github.com>
2026-02-22 10:11:25 +0500

    Update MainActivity.kt

--- app/src/main/java/com/jarvis/lite/MainActivity.kt
+++ app/src/main/java/com/jarvis/lite/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.jarvis.lite
 
-import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val tv = TextView(this)
         tv.text = "Jarvis Lite Active"
         tv.textSize = 24f
+
         setContentView(tv)
     }
 }
